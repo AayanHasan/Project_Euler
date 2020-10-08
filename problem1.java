8073b444cceb354c6c556d3a571df3960f1eb767
AayanHasan <53379121+AayanHasan@users.noreply.github.com>
2020-10-09 01:29:34 +0530

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,3 +1,5 @@
+/*if we list all the natural numbers below 10 that are multiple of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
+find the sum of all the multiples of 3 or 5 below 1000. */
 package project_euler;
 
 public class problem1 {
